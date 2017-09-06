8c21b7143c2ca0031579fc34fa1b606786249bb5
nippumanni1 <nippumanni1@gmail.com>
2017-09-05 22:45:54 -0500

    Initial commit. September 5

---
+++ app/src/main/java/com/example/daleshprashar/x_network_t_7/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.daleshprashar.x_network_t_7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//https://jsonparsingdemo-cec5b.firebaseapp.com/jsonData/moviesData.txt
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
